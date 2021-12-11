259e44e8f23f20b6c9a620aa85d255ce18238203
cedricmendoza02 <cedric.mendoza02@gmail.com>
2021-12-11 14:24:22 +0800

    Removed the onclick status from NoteItem component

--- src/components/noteslist/NotesList.jsx
+++ src/components/noteslist/NotesList.jsx
@@ -2,18 +2,10 @@ import React, { useState } from 'react';
 import './noteslist.css';
 
 function NoteItem({title, content, onClick}) {
-  const [status, setStatus] = useState(false);
-
-  function handleClick() {
-    setStatus(!status);
-    onClick();
-  }
   return (
     <li 
       className="note-item" 
-      style={status ? {backgroundColor: "red", color: "white"}
-      : {}}
-      onClick={handleClick}>{title}</li>
+      onClick={onClick}>{title}</li>
   )
 }
 
